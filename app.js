0fda5aead451bad6298a26d48190cdb13a90c8f0
abdullah <abdullamsuleman@gmail.com>
2026-07-03 16:08:01 +0300

    JS update

---
+++ app.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#collect-button")
+const pearlCounter = document.querySelector("#pearl-count")
+const message = document.querySelector("#message")
+
+let pearlCount = 0
+
+button.addEventListener("click", function(){
+    
+})
+
